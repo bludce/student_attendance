1fc1da158e421ffa877a91260776cc8225bfdd1d
Aleksey Zadorozhny <alekszadorozhny90@gmail.com>
2020-04-06 11:35:32 +0300

    init: init react boilerplate

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React, {Fragment} from "react";
+import './index.sass'
+
+const App = () => {
+  return (
+    <Fragment>
+      
+      adgsdgds
+
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ client/webpack.config.js
@@ -0,0 +1,60 @@
+/* eslint-disable */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (argv) => ({
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: {
+                path: './postcss.config.js',
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+});
