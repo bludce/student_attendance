d2dc0ad2bcd98946b21d849b661ed96a844a7cbf
Aleksey Zadorozhny <alekszadorozhny90@gmail.com>
2020-04-23 11:07:55 +0300

    feat: add logout button

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Redirect } from 'react-router-dom';
+
 
 import './Header.sass'
 
-const Header = (props) => (
-  <header className="header">
-      <nav>
-        <ul className="nav">
-          {props.links.map((post) =>
-            <li className="nav__item" key={post.name+post.link}>
-              <NavLink exact activeClassName="nav__link--active" className="nav__link" to={post.link}>
-              {post.name}
-            </NavLink>
+const Header = (props) => {
+  const logout = () => {
+    localStorage.setItem('role', '')
+  }
+  return (
+    <header className="header">
+        <nav className="flex">
+          <ul className="nav">
+            {props.links.map((post) =>
+              <li className="nav__item" key={post.name+post.link}>
+                <NavLink exact activeClassName="nav__link--active" className="nav__link" to={post.link}>
+                {post.name}
+              </NavLink>
+              </li>
+            )}
+            <li className="nav__item" key="logout">
+              <NavLink exact  className="nav__link" to="/login" onClick={logout}>
+                Выход
+              </NavLink>
             </li>
-          )}
-        </ul>
-      </nav>   
-  </header>
-)
+          </ul>
+        </nav>    
+    </header>
+)}
 
 export default Header
